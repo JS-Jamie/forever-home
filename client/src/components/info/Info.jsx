510b3d2e78e780fbbbc51ae8acc60dd6d368b55b
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2023-01-27 23:45:10 -0800

    Add getInvolved component to home page

--- client/src/components/info/Info.jsx
+++ client/src/components/info/Info.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <Container>
+      <hr />
+      <Col>
+        <Row>
+          <h2>Adoption Hours</h2>
+          <h2>Adoption Hours</h2>
+        </Row>
+      </Col>
+      <Col>
+        <Row>
+          <h1>Get updates and inspiring stories. Sign up for news.</h1>
+        </Row>
+        <button>SIGN UP</button>
+      </Col>
+      <Col>
+        <Row></Row>
+      </Col>
+    </Container>
+  );
 };
 
 export default Info;
--- client/src/components/volunteerAndFoster/VolunteerAndFoster.jsx
+++ client/src/components/volunteerAndFoster/VolunteerAndFoster.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import './volunteerAndFoster.css';
+import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
 
 const VolunteerAndFoster = () => {
-  return <div>VolunteerAndFoster</div>;
+  return (
+    <Container>
+      <h1 className='getInvolvedText'>Get Involved</h1>
+      <Row xs={1} md={2} className='g-4'>
+        <div>
+          <Card className='w-100'>
+            <Card.Img
+              className='w-100 cardImg'
+              variant='top'
+              src='https://images.unsplash.com/photo-1526976668912-1a811878dd37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHZvbHVudGVlcnxlbnwwfDB8MHx8&auto=format&fit=crop&w=400&q=60'
+            />
+            <Card.Body>
+              <Card.Title>VOLUNTEER</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          <Card className='w-100'>
+            <Card.Img
+              className='w-100 cardImg'
+              variant='top'
+              src='https://images.unsplash.com/photo-1601758261049-55d060e1159a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nJTIwYW5kJTIwcGVvcGxlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=400&q=60'
+            />
+            <Card.Body>
+              <Card.Title>FOSTER</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </Row>
+    </Container>
+  );
 };
 
 export default VolunteerAndFoster;
