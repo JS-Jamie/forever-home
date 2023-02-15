297cb8a3cad7331d6d8044f733d48048dc88a6d7
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2023-02-14 23:58:18 -0800

    Complete animalController file

--- server/controllers/animalController.js
+++ server/controllers/animalController.js
@@ -8,5 +8,18 @@ const getAnimals = asyncHandler(async (req, res) => {
   const pageSize = 12;
   const page = Number(req.query.pageNumber) || 1;
 
-  const count = await Animal.countDocuments();
+  res.json({ animals, page, pages: Math.ceil(count / pageSize) });
+});
+
+//@desc   Fetch single animal
+//@route   GET / api/animals/:id
+const getAniamlById = asyncHandler(async (req, res) => {
+  const animal = await Animal.findById(req.params.id);
+
+  if (animal) {
+    res.json(animal);
+  } else {
+    res.status(404);
+    throw new Error('Animal not found');
+  }
 });
