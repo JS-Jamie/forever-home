70201271be6e5e9b27d8da0a7092d1305e1a0c41
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2023-02-25 23:32:47 -0800

    Add handleClick functionality and a minor CSS feature

--- client/src/components/volunteerAndFoster/VolunteerAndFoster.jsx
+++ client/src/components/volunteerAndFoster/VolunteerAndFoster.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './volunteerAndFoster.css';
 import { Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 
 const VolunteerAndFoster = () => {
+  const navigate = useNavigate();
+
+  const handleClickVolunteer = () => {
+    navigate('/volunteer');
+  };
+
+  const handleClickForster = () => {
+    navigate('/foster');
+  };
+
   return (
     <Container className='getInvolvedSection'>
       <h1 className='getInvolvedText'>Get Involved</h1>
@@ -13,11 +24,19 @@ const VolunteerAndFoster = () => {
           <Card className='w-100'>
             <Card.Img
               className='w-100 cardImg'
+              style={{ cursor: 'pointer' }}
+              onClick={handleClickVolunteer}
               variant='top'
               src='https://images.unsplash.com/photo-1526976668912-1a811878dd37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHZvbHVudGVlcnxlbnwwfDB8MHx8&auto=format&fit=crop&w=400&q=60'
             />
             <Card.Body>
-              <Card.Title className='cardTitle'>VOLUNTEER ➤</Card.Title>
+              <Card.Title
+                className='cardTitle'
+                style={{ cursor: 'pointer' }}
+                onClick={handleClickVolunteer}
+              >
+                VOLUNTEER ➤
+              </Card.Title>
             </Card.Body>
           </Card>
         </div>
@@ -25,11 +44,19 @@ const VolunteerAndFoster = () => {
           <Card className='w-100'>
             <Card.Img
               className='w-100 cardImg'
+              style={{ cursor: 'pointer' }}
+              onClick={handleClickForster}
               variant='top'
               src='https://images.unsplash.com/photo-1601758261049-55d060e1159a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nJTIwYW5kJTIwcGVvcGxlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=400&q=60'
             />
             <Card.Body>
-              <Card.Title className='cardTitle'>FOSTER ➤</Card.Title>
+              <Card.Title
+                className='cardTitle'
+                style={{ cursor: 'pointer' }}
+                onClick={handleClickForster}
+              >
+                FOSTER ➤
+              </Card.Title>
             </Card.Body>
           </Card>
         </div>
