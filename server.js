b0b460ce90f63d6756242e69b7cc24294424fcf1
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2023-03-01 11:35:20 -0800

    Change package.json in both front and back ends and update server.js and favicon before deployment

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import animalRoutes from './routes/animalRoutes.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -19,10 +25,10 @@ app.use(express.json());
 app.use('/api/animals', animalRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './frontend/build')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public') + '/index.html')
   );
 } else {
   app.get('/', (req, res) => {
@@ -34,7 +40,7 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(
   PORT,
